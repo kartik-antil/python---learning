dbac0e9fbd5d4b8cfdb42a6a85f4f0ed301499b6
Kartik Antil <your_email@example.com>
2026-08-22 12:55:45 +0530

    docs : add modules , comments and escape sequence chracters

---
+++ 02_modules&comments.py
@@ -0,0 +1,17 @@
+#modules has two types - 1. built in modules 2. external modules 
+#comments and escape sequence chracter 
+print("hey,I am a good boy")
+#for new line we have to add \n 
+print("hey,I am a good boy \nand this viewer is also a good boy")
+#comments uses for those line which we do not want to run
+#using ctrl+forward slash(/) its comments more than one lines 
+#multiline comments
+#we  also used ( ``´ ) single triple quotes or ( """ ) double triple quotes to multi line comments
+"""
+kartik learn python by code with harry
+100 days code series 
+python automation
+"""
+#ESCAPE SEQUENCE CHRACTER( \") BACK SLASH DOUBLE QUOTE 
+print("hey i am a \"good boy\" and this viewer is also a good boy")
+
