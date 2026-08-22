dc559a8c0741b5f808dda14cd5e72fe054546555
Kartik Antil <your_email@example.com>
2026-08-22 10:46:53 +0530

    feat : add day 1 python basics

---
+++ 01_hello_world.py
@@ -0,0 +1,10 @@
+#guide van roesum discover python in 1989
+#python is a open source language
+#it provides big library support
+print("hello world")
+print(5)
+print("hello kartik ")
+print(10*3)
+print("kartik , how are you")
+print("kartik,100")
+print("bye")
