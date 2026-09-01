b889f352ccb9a7f504ab175339e67168b1b600ab
kartik-antil <antilkartik680@gmail.com>
2026-09-01 21:00:40 +0530

    docs:c_language

---
+++ c_language.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int void_func()
+{
+    int var=20;
+    printf("value of var=%d \n",var);
+    return 0;
+}
+
+#include<stdio.h>
+int main()
+{
+    void_func();
+    printf("hello world");
+    return 0;
+}
