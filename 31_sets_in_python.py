9f70c74a59a41e0fbd8bc3344125e99560ea94d7
kartik-antil <antilkartik680@gmail.com>
2026-09-02 20:01:09 +0530

    docs:sets in python

---
+++ 31_sets_in_python.py
@@ -0,0 +1,19 @@
+#unordered collection
+s={2,4,2,4,2,6}
+print(s)
+
+info={"carla",19,False,5.9,19}
+print(info)
+
+kartik={}
+print(type(kartik))
+
+kartik=set()
+print(type(kartik))
+
+for value in info:
+    print(info)
+
+for value in info:
+    print(value)
+    
