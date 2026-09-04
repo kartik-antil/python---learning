74489b8fd21a2ce48ad0dbf00d1191e804485f97
kartik-antil <antilkartik680@gmail.com>
2026-09-04 18:07:25 +0530

    doc:float,char,var

---
+++ c_float_double_float_char.c
@@ -0,0 +1,14 @@
+
+#include<stdio.h>
+int main()
+{
+    int var1=20;
+    printf("value of var=%d \n",var1);
+    printf("\n");
+
+    int var2;
+    printf("enter value:");
+    scanf("%d",&var2);
+    printf("value of var2=%d \n",var2);
+    return 0;
+}
