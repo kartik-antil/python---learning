6f3ddf57a860e24adc2436dbb20eb033e77677da
kartik-antil <antilkartik680@gmail.com>
2026-09-14 18:18:45 +0530

    feat:switch statement

---
+++ my.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+    int day=3;
+    switch (day)
+    {
+        case 1:
+    printf("today is monday\n");
+        break;
+
+            case 2:
+            printf("today is tuesday\n");
+            break;
+
+            case 3:
+            printf("today is wednesday\n");
+            break;
+            case 4:
+            printf("today is thursday\n");
+            break;
+
+            case 5:
+            printf("today is friday\n");
+            break;
+
+            default:
+            printf("weekend\n");
+            break;
+
+
+
+
+    }
+            return 0;
+}
